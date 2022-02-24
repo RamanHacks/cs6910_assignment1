stabilize training with softmax fix, fix softmax backward, save best model

numpy/cuDL/activations/__init__.py
@@ -62,11 +62,30 @@ class Softmax(Activation):
         self.name = "softmax"
 
     def forward(self, x):
+        # https://cs231n.github.io/linear-classify/#softmax
+        # using a normalizing trick to avoid overflow
+        # https://stackoverflow.com/questions/42599498/numercially-stable-softmax/42606665#42606665
+        x -= np.max(x, axis=1, keepdims=True)
+        x = np.exp(x) / np.exp(x).sum(axis=1, keepdims=True)
         self.output = x
-        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+        return x
 
     def backward(self):
-        return np.ones(self.output.shape)
+
+        # https://www.bragitoff.com/2021/12/efficient-implementation-of-softmax-activation-function-and-its-derivative-jacobian-in-python/
+        """Returns the jacobian of the Softmax function for the given set of inputs.
+        Inputs:
+        x: should be a 2d array where the rows correspond to the samples
+            and the columns correspond to the nodes.
+        Returns: jacobian
+        """
+        s = self.output
+        a = np.eye(s.shape[-1])
+        temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]), dtype=np.float32)
+        temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]), dtype=np.float32)
+        temp1 = np.einsum("ij,jk->ijk", s, a)
+        temp2 = np.einsum("ij,ik->ijk", s, s)
+        return temp1 - temp2
 
     def __call__(self, x):
         return self.forward(x)

numpy/cuDL/layers/Dense.py
@@ -65,7 +65,7 @@ class Dense(Layer):
 
         return outputs
 
-    def backward(self, pre_grad, *args, **kwargs):
+    def backward(self, pre_grad, regularizer=None, *args, **kwargs):
         # sanity check
         assert isinstance(pre_grad, np.ndarray), "pre_grad must be of type np.ndarray"
         assert (
@@ -74,10 +74,22 @@ class Dense(Layer):
         assert pre_grad.shape[0] > 0, f"pre_grad must have at least one row"
 
         # compute gradients
-        d_activation = pre_grad * self.activation.backward()
+        d_activation = self.activation.backward()
+
+        # check if pred_grad and d_activation have dimesions
+        if pre_grad.ndim != d_activation.ndim:
+            # for softmax activation
+            d_activation = np.einsum("ijk,ik->ij", d_activation, pre_grad)
+        else:
+            d_activation = pre_grad * d_activation
 
         self.d_weights = self._input.T.dot(d_activation)
         self.d_bias = d_activation.mean(axis=0)
+
+        # regularization gradients
+        if regularizer is not None:
+            self.d_weights += regularizer.backward(self.weights)
+            self.d_bias += regularizer.backward(self.bias)
 
         # propagate gradients to lower layers
         if not self.is_first_layer:

numpy/cuDL/model.py
@@ -6,6 +6,7 @@ from cuDL.loss import get_loss
 from cuDL.optim import get_optimizer
 from cuDL.activations import get_activation
 from cuDL.metrics import get_metric
+from cuDL.regularizers import get_regularizer
 import cuDL.metrics as metrics
 
 from sklearn.model_selection import train_test_split
@@ -21,7 +22,9 @@ warnings.filterwarnings("ignore")
 
 
 class Model:
-    def __init__(self, name="", layers=None, loss=None, optimizer=None):
+    def __init__(
+        self, name="", layers=None, loss=None, optimizer=None, model_save_path=None
+    ):
 
         self.name = name
         self.layers = layers
@@ -42,6 +45,8 @@ class Model:
             self.optimizer = None
 
         self.metrics = None
+        self.regularizer = None
+        self.model_save_path = model_save_path
 
     def add_layer(self, layer):
         if self.layers is None:
@@ -52,7 +57,15 @@ class Model:
         self.layers.append(layer)
 
     # TODO: change loss and optimizer to some useful defaults
-    def compile(self, loss=None, optimizer=None, learning_rate=0.001, metrics=None):
+    def compile(
+        self,
+        loss=None,
+        optimizer=None,
+        learning_rate=0.001,
+        metrics=None,
+        regularizer=None,
+        weight_decay_rate=0.0,
+    ):
         if loss is None and self.loss is None:
             raise ValueError(
                 "No loss specified, You should specify a loss during model init or compile"
@@ -67,6 +80,11 @@ class Model:
             raise ValueError(
                 "No layers specified, You should specify at least one layer during model init with model add_layer before compile"
             )
+
+        if regularizer is not None:
+            assert isinstance(regularizer, str), "Regularizer name must be a string"
+            # assert weight_decay_rate , "Weight decay rate must be greater than 0"
+            self.regularizer = get_regularizer(regularizer, weight_decay_rate)
 
         self.loss = loss
         self.optimizer = optimizer
@@ -124,6 +142,17 @@ class Model:
         seed=42,
         shuffle=True,
     ):
+        best_loss = np.inf
+        best_metric = 0.0
+
+        if self.model_save_path is None:
+            self.model_save_path = "./models/ckpt.bin"
+            print(
+                "No model save path specified, saving to default path: {}".format(
+                    self.model_save_path
+                )
+            )
+
         def clear_lists():
             self.mean_train_loss = []
             self.mean_val_loss = []
@@ -206,10 +235,13 @@ class Model:
                             metric_func(y_batch, y_pred)
                         )
 
+            train_loss = np.mean(self.mean_train_loss)
+            val_loss = np.mean(self.mean_val_loss)
+
             printing_dict = {
                 "epoch": epoch + 1,
-                "train_loss": round(np.mean(self.mean_train_loss), 2),
-                "val_loss": round(np.mean(self.mean_val_loss), 2),
+                "train_loss": round(train_loss, 2),
+                "val_loss": round(val_loss, 2),
             }
             # print(self.metrics)
             if self.metrics is not None and self.metrics != []:
@@ -219,6 +251,18 @@ class Model:
                     )
             # adds metrics to tqdm progress bar instead of printing
             tk.set_postfix(printing_dict)
+
+            # save best model
+            if val_loss < best_loss:
+                best_loss = val_loss
+                # best_metric = printing_dict[self.metrics[0][0]]
+                # print best metric with 2 decimal places
+                print(
+                    f"Val loss improved from {best_loss:.2f} to {val_loss:.2f}, saving model"
+                )
+                if self.model_save_path is not None:
+                    self.save(self.model_save_path)
+
             # clear the lists for the next epoch
             clear_lists()
 
@@ -231,7 +275,7 @@ class Model:
     def backward(self, y_true, y_pred, *args, **kwargs):
         next_grad = self.loss.backward(y_true, y_pred)
         for layer in self.layers[::-1]:
-            next_grad = layer.backward(next_grad)
+            next_grad = layer.backward(next_grad, self.regularizer)
 
         params = []
         grads = []
@@ -243,7 +287,10 @@ class Model:
 
     def compute_loss(self, y_true, y_pred):
         assert self.loss is not None, "No loss specified"
-        return self.loss.forward(y_true, y_pred)
+        loss = self.loss.forward(y_true, y_pred)
+        if self.regularizer is not None:
+            loss += self.regularizer(self.params)
+        return loss
 
     def update(self, params, grads):
         assert self.optimizer is not None, "No optimizer specified"
@@ -253,7 +300,7 @@ class Model:
         total_params = 0
         # print a summary of the model like keras model_summary
         print("Model Summary")
-        print("=" * 20)
+        print("=" * 30)
         print(f"Layers: {len(self.layers)}")
         for idx, layer in enumerate(self.layers):
             print(f"Layer {idx}: {layer.__class__.__name__}", end="\t")
@@ -263,7 +310,7 @@ class Model:
             total_params += layer.num_params
 
         print(f"Total params: {millify(total_params)}")
-        print("=" * 20)
+        print("=" * 30)
 
     def save(self, path):
         joblib.dump(self, path)
@@ -272,6 +319,13 @@ class Model:
     def load(self, path):
         self = joblib.load(path)
         return self
+
+    @property
+    def params(self):
+        params = []
+        for layer in self.layers:
+            params += layer.params
+        return params
 
     def __str__(self):
         return "Model: {}\nLayers: {}\nLoss: {}\nOptimizer: {}".format(

numpy/train_fmnist.py
@@ -98,15 +98,15 @@ if __name__ == "__main__":
         help="Number of layers in the model",
     )
     parser.add_argument(
-        "--hidden_size",
-        type=int,
-        default=128,
-        help="Number of hidden units in the model",
+        "--hidden_sizes",
+        type=str,
+        default="128",
+        help="this is an integer or a comma-separated list of integers indicating the number of hidden units in each layer of the model.",
     )
     parser.add_argument(
         "--weight_decay_rate",
         type=float,
-        default=0.0,
+        default=0.0005,
         help="Weight decay rate (L2 regularizer) for training",
     )
     parser.add_argument(
@@ -142,12 +142,19 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", default=False)
     args = parser.parse_args()
 
+    str_hidden_sizes = args.hidden_sizes
+    args.hidden_sizes = [int(x) for x in args.hidden_sizes.split(",")]
+    # if only one number is passed, that means user wants to use the same number of hidden units in each layer
+    if len(args.hidden_sizes) == 1:
+        args.hidden_sizes = [args.hidden_sizes[0]] * args.num_layers
+    assert len(args.hidden_sizes) == args.num_layers
+
     if args.model_dir is None and not args.debug:
         # set a descriptive model directory name
         model_args = [
             "fmnist",
             "L_{}".format(args.num_layers),
-            "HS_{}".format(args.hidden_size),
+            "HS_{}".format(str_hidden_sizes),
             "DR_{}".format(args.weight_decay_rate),
             "O_{}".format(args.optimizer),
             "loss_{}".format(args.loss),
@@ -156,8 +163,11 @@ if __name__ == "__main__":
         args.model_dir = os.path.join(
             f"models/{model_str}",
         )
-        print("Saving models here: {}".format(args.model_dir))
-        os.makedirs(args.model_dir)
+        os.makedirs(args.model_dir, exist_ok=True)
+
+        save_path = os.path.join(args.model_dir, "ckpt.bin")
+
+        print("Saving models here: {}".format(save_path))
     # Set the random seed for reproducibility
     np.random.seed(args.seed)
 
@@ -183,22 +193,22 @@ if __name__ == "__main__":
         plot_images(x_train, y_train)
 
     # Build the model
-    model = Model()
+    model = Model(model_save_path=save_path)
 
     model.add_layer(
         Dense(
             input_dim=x_train.shape[1],
-            output_dim=args.hidden_size,
+            output_dim=args.hidden_sizes[0],
             activation=args.activation,
             init_method=args.weight_init,
         )
     )
 
-    for _ in range(args.num_layers - 2):  # exclude the first and last layer
+    for idx in range(args.num_layers - 1):  # exclude the first layer
         model.add_layer(
             Dense(
-                input_dim=args.hidden_size,
-                output_dim=args.hidden_size,
+                input_dim=args.hidden_sizes[idx],
+                output_dim=args.hidden_sizes[idx + 1],
                 activation=args.activation,
                 init_method=args.weight_init,
             )
@@ -207,7 +217,7 @@ if __name__ == "__main__":
     if args.loss == "cross_entropy":
         model.add_layer(
             Dense(
-                input_dim=args.hidden_size,
+                input_dim=args.hidden_sizes[-1],
                 output_dim=10,
                 activation="softmax",
                 init_method=args.weight_init,
@@ -234,6 +244,8 @@ if __name__ == "__main__":
         optimizer=optimizer,
         learning_rate=args.learning_rate,
         metrics=["accuracy"],
+        regularizer="L2",
+        weight_decay_rate=args.weight_decay_rate,
     )
 
     # Train the model
@@ -246,6 +258,12 @@ if __name__ == "__main__":
         shuffle=args.shuffle,
         seed=args.seed,
     )
+
+    print("=" * 30)
+    print("Evaluating model on test data with best model")
+
+    # load the best model
+    model.load(model.model_save_path)
 
     # Evaluate the model
     model.predict(x_test, y_test, print_classification_metrics=True)