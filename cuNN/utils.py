81b609f6e63f2b0ea2aa512f3b46e7c88954957d
Abhigyan Raman <ramanabhigyan@gmail.com>
2022-02-25 18:19:01 +0530

    added train.py

---
+++ cuNN/utils.py
@@ -0,0 +1,257 @@
+"""Code is based on
+https://github.com/rwightman/pytorch-image-models/blob/master/timm/data/mixup.py
+"""
+import cupy as cp
+import numpy as np
+import pickle
+from PIL import Image, ImageOps, ImageEnhance
+
+def save(file, name):
+    with open(name, 'wb') as f:
+        pickle.dump(file, f)
+
+def load(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
+   
+def smooth_batch(target, smoothing=0.1, num_classes=10):
+    target *= (1. - smoothing)
+    target += (smoothing / num_classes)
+    return target
+
+
+def mix_target(target_a, target_b, lam, num_classes=10, smoothing=0.):
+    y1 = smooth_batch(target_a, smoothing, num_classes)
+    y2 = smooth_batch(target_b, smoothing, num_classes)
+    return lam * y1 + (1. - lam) * y2 
+
+def rand_bbox(size, lam):
+    W = size[1]
+    H = size[2]
+    cut_rat = cp.sqrt(1. - lam)
+    cut_w = cp.int(W * cut_rat)
+    cut_h = cp.int(H * cut_rat)
+
+    # uniform
+    cx = cp.random.randint(W)
+    cy = cp.random.randint(H)
+
+    bbx1 = cp.clip(cx - cut_w // 2, 0, W)
+    bby1 = cp.clip(cy - cut_h // 2, 0, H)
+    bbx2 = cp.clip(cx + cut_w // 2, 0, W)
+    bby2 = cp.clip(cy + cut_h // 2, 0, H)
+
+    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (size[1] * size[2]))
+
+    return bbx1, bby1, bbx2, bby2, lam
+
+
+def cutmix_batch(image, target, prob, beta=.2, num_classes=10):
+    if cp.random.rand(1) < prob:
+        lam = cp.random.beta(beta, beta)
+    else:
+        return image, target
+
+    rand_index = cp.random.permutation(image.shape[0])
+    target_a = target
+    target_b = target[rand_index]
+    bbx1, bby1, bbx2, bby2, lam = rand_bbox(image.shape, lam)
+    image[:, bbx1:bbx2, bby1:bby2] = image[rand_index, bbx1:bbx2,
+                                              bby1:bby2]
+    target = mix_target(target_a,target_b,lam)
+    return image, target
+
+
+def mixup_batch(image, target, prob, beta=.2, num_classes=10):
+    if cp.random.rand(1) < prob:
+        lam = cp.random.beta(beta, beta)
+    else:
+        return image, target
+
+    rand_index = cp.random.permutation(image.shape[0])
+    target_a = target
+    target_b = target[rand_index]
+    
+    image_a = image
+    image_b = image[rand_index]
+    
+    image = mix_target(image_a,image_b,lam)
+    target = mix_target(target_a,target_b,lam)
+    return image, target
+
+
+##############################################################################
+
+
+
+def int_parameter(level, maxval):
+    """Helper function to scale `val` between 0 and maxval .
+    Args:
+    level: Level of the operation that will be between [0, `PARAMETER_MAX`].
+    maxval: Maximum value that the operation can have. This will be scaled to
+      level/PARAMETER_MAX.
+    Returns:
+    An int that results from scaling `maxval` according to `level`.
+    """
+    return int(level * maxval / 10)
+
+
+def float_parameter(level, maxval):
+    """Helper function to scale `val` between 0 and maxval.
+    Args:
+    level: Level of the operation that will be between [0, `PARAMETER_MAX`].
+    maxval: Maximum value that the operation can have. This will be scaled to
+      level/PARAMETER_MAX.
+    Returns:
+    A float that results from scaling `maxval` according to `level`.
+    """
+    return float(level) * maxval / 10.
+
+
+def sample_level(n):
+    return np.random.uniform(low=0.1, high=n)
+
+
+def autocontrast(pil_img, _):
+    return ImageOps.autocontrast(pil_img)
+
+
+def equalize(pil_img, _):
+    return ImageOps.equalize(pil_img)
+
+
+def posterize(pil_img, level):
+    level = int_parameter(sample_level(level), 4)
+    return ImageOps.posterize(pil_img, 4 - level)
+
+
+def rotate(pil_img, level):
+    degrees = int_parameter(sample_level(level), 30)
+    if np.random.uniform() > 0.5:
+        degrees = -degrees
+    return pil_img.rotate(degrees, resample=Image.BILINEAR)
+
+
+def solarize(pil_img, level):
+    level = int_parameter(sample_level(level), 256)
+    return ImageOps.solarize(pil_img, 256 - level)
+
+
+def shear_x(pil_img, level):
+    level = float_parameter(sample_level(level), 0.3)
+    if np.random.uniform() > 0.5:
+        level = -level
+    return pil_img.transform(pil_img.size,
+                           Image.AFFINE, (1, level, 0, 0, 1, 0),
+                           resample=Image.BILINEAR)
+
+
+def shear_y(pil_img, level):
+    level = float_parameter(sample_level(level), 0.3)
+    if np.random.uniform() > 0.5:
+        level = -level
+    return pil_img.transform(pil_img.size,
+                           Image.AFFINE, (1, 0, 0, level, 1, 0),
+                           resample=Image.BILINEAR)
+
+
+def translate_x(pil_img, level):
+    level = int_parameter(sample_level(level), pil_img.size[0] / 3)
+    if np.random.random() > 0.5:
+        level = -level
+    return pil_img.transform(pil_img.size,
+                           Image.AFFINE, (1, 0, level, 0, 1, 0),
+                           resample=Image.BILINEAR)
+
+
+def translate_y(pil_img, level):
+    level = int_parameter(sample_level(level), pil_img.size[0] / 3)
+    if np.random.random() > 0.5:
+        level = -level
+    return pil_img.transform(pil_img.size,
+                           Image.AFFINE, (1, 0, 0, 0, 1, level),
+                           resample=Image.BILINEAR)
+
+
+# operation that overlaps with ImageNet-C's test set
+def color(pil_img, level):
+    level = float_parameter(sample_level(level), 1.8) + 0.1
+    return ImageEnhance.Color(pil_img).enhance(level)
+
+
+# operation that overlaps with ImageNet-C's test set
+def contrast(pil_img, level):
+    level = float_parameter(sample_level(level), 1.8) + 0.1
+    return ImageEnhance.Contrast(pil_img).enhance(level)
+
+
+# operation that overlaps with ImageNet-C's test set
+def brightness(pil_img, level):
+    level = float_parameter(sample_level(level), 1.8) + 0.1
+    return ImageEnhance.Brightness(pil_img).enhance(level)
+
+
+# operation that overlaps with ImageNet-C's test set
+def sharpness(pil_img, level):
+    level = float_parameter(sample_level(level), 1.8) + 0.1
+    return ImageEnhance.Sharpness(pil_img).enhance(level)
+
+
+augmentations = [
+    autocontrast, equalize, posterize, rotate, solarize, shear_x, shear_y,
+    translate_x, translate_y
+]
+
+augmentations_all = [
+    autocontrast, equalize, posterize, rotate, solarize, shear_x, shear_y,
+    translate_x, translate_y, color, contrast, brightness, sharpness
+]
+
+def normalize(image):
+    """Normalize input image channel-wise to zero mean and unit variance."""
+    return image - 127
+
+def apply_op(image, op, severity):
+    #   image = np.clip(image, 0, 255)
+    pil_img = Image.fromarray(image)  # Convert to PIL.Image
+    pil_img = op(pil_img, severity)
+    return np.asarray(pil_img)
+
+def augment_and_mix(image, severity=3, width=3, depth=-1, alpha=.2, prob=1.):
+    """Perform AugMix augmentations and compute mixture.
+    Args:
+    image: Raw input image as float32 np.ndarray of shape (h, w, 1)
+    severity: Severity of underlying augmentation operators (between 1 to 10).
+    width: Width of augmentation chain
+    depth: Depth of augmentation chain. -1 enables stochastic depth uniformly
+      from [1, 3]
+    alpha: Probability coefficient for Beta and Dirichlet distributions.
+    Returns:
+    mixed: Augmented and mixed image.
+    """
+    image = image[:, :, np.newaxis]
+    image = np.repeat(image, 3, -1) 
+    if np.random.rand(1) < prob:
+        ws = np.float32(
+        np.random.dirichlet([alpha] * width))
+        m = np.float32(np.random.beta(alpha, alpha))
+    else:
+        return image
+    
+    mix = np.zeros_like(image).astype(np.float32)
+    for i in range(width):
+        image_aug = image.copy()
+        depth = depth if depth > 0 else np.random.randint(1, 4)
+        for _ in range(depth):
+            op = np.random.choice(augmentations)
+            image_aug = apply_op(image_aug, op, severity)
+        # Preprocessing commutes since all coefficients are convex
+        mix += ws[i] * image_aug
+
+    mixed = (1 - m) * image + m * mix
+    return mixed
+
+def augmix_batch(image_list, severity=3, width=3, depth=-1, alpha=.2, prob=1.):
+    from joblib import Parallel, delayed
+    batch_out = Parallel(n_jobs=12)(delayed(augment_and_mix)(i,severity,width,depth,alpha,prob) for i in image_list)
+    return batch_out
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 from cuNN.optimizer import Momentum, SGD, AdaGrad, AdamW, RMSProp, Adam, Nadam
 from cuNN.module import SimpleLinear
 from cuNN.loss import CrossEntropy, MSE
-from cuNN.utils import save, load, cutmix_batch, smooth_batch, mixup_batch
+from cuNN.utils import save, load, cutmix_batch, smooth_batch, mixup_batch, augmix_batch
 
 
 def setup_data(val_split, seed):
@@ -82,8 +82,12 @@ def main(args):
         if args.enable_wandb:
             wandb.init(config=args)
     
-    model = SimpleLinear(784, 10, args.hidden_sizes, args.num_layers,
-                args.activation, args.weight_init, args.regularizer, args.weight_decay_rate)
+    if args.augmix_prob > 0:
+        model = SimpleLinear(784*3, 10, args.hidden_sizes, args.num_layers,
+                    args.activation, args.weight_init, args.regularizer, args.weight_decay_rate)
+    else:
+        model = SimpleLinear(784, 10, args.hidden_sizes, args.num_layers,
+                    args.activation, args.weight_init, args.regularizer, args.weight_decay_rate)
 
     if args.loss == "cross_entropy":
         criterion = CrossEntropy()
@@ -105,13 +109,6 @@ def main(args):
     elif args.optimizer == "nadam":
         opt = Nadam(model.parameters, lr=args.learning_rate)
     
-    tot_samples = len(x_train)
-    batch_size = args.batch_size
-    ep_acc, ep_loss = None, None 
-    epoch_log = tqdm(total=args.epochs, desc='Epoch', position=0, unit='epoch', leave=True)
-    step_no =0
-    best_val_acc = 0
-    
     (x_train_og, y_train), (x_val, y_val), (x_test, y_test) = setup_data(args.val_split, args.seed)
     x_train = x_train_og.copy()
     
@@ -120,24 +117,38 @@ def main(args):
     y_val = cp.asarray(y_val)
     y_test = cp.asarray(y_test)
     
+    if args.augmix_prob > 0:
+        x_val = x_val[:, :, :, np.newaxis]
+        x_val = np.repeat(x_val, 3, -1) 
+        x_test = x_test[:, :, :, np.newaxis]
+        x_test = np.repeat(x_test, 3, -1) 
+        
     x_test = x_test.reshape(1,x_test.shape[0],-1).squeeze() / 255.0
     x_val = x_val.reshape(1,x_val.shape[0],-1).squeeze() / 255.0
-    
+
+    tot_samples = len(x_train)
+    batch_size = args.batch_size
+    ep_acc, ep_loss = None, None 
+    epoch_log = tqdm(total=args.epochs, desc='Epoch', position=0, unit='epoch', leave=True)
+    step_no =0
+    best_val_acc = 0
     for epoch in range(args.epochs):
         if args.shuffle:
             index = np.random.permutation(tot_samples)
         else:
             index = np.arange(tot_samples)
-        
+            
+        if args.augmix_prob > 0:
+            x_train = augmix_batch(x_train_og, prob=args.augmix_prob)
+            
         sum_loss, sum_acc = 0, 0
         for start_idx in range(0, tot_samples, batch_size):
             step_no +=1
             if start_idx + batch_size > tot_samples:
                 end_idx = tot_samples
-                len_t = tot_samples - start_idx
             else:
                 end_idx = start_idx + batch_size 
-                len_t = batch_size
+            len_t = end_idx - start_idx
                 
             batch_index = index[start_idx:end_idx]
             x_batch = cp.asarray(np.array([x_train[i] for i in batch_index])) / 255.0
@@ -147,11 +158,10 @@ def main(args):
                 y_batch = smooth_batch(y_batch, smoothing=args.smoothing_val)
             if args.epochs - epoch > args.epochs/2 and args.cutmix_prob > 0:
                 x_batch, y_batch = cutmix_batch(x_batch, y_batch, prob=args.cutmix_prob)
-            elif args.epochs - epoch > args.epochs/2 and args.mixup_prob > 0:
+            if args.epochs - epoch > args.epochs/2 and args.mixup_prob > 0:
                 x_batch, y_batch = mixup_batch(x_batch, y_batch, prob=args.mixup_prob)
                 
-            x_batch = x_val.reshape(1,len_t,-1).squeeze()
-            
+            x_batch = x_batch.reshape(1,len_t,-1).squeeze()
             y = model(x_batch)
             loss = criterion(y, y_batch)
             model.backward(loss)
@@ -186,7 +196,8 @@ def main(args):
                     'best_val_acc': float(cp.round(best_val_acc,3)),
                 }  
             wandb.log(printing_dict)
-        
+    epoch_log.close()
+    
     # testing accuracy
     y_test_pred = model.forward((x_test), True)
     test_acc = cp.mean(y_test_pred.argmax(axis=1) == cp.array(y_test).argmax(axis=1))
@@ -204,3 +215,107 @@ def main(args):
     if enable_wandb:
         wandb.log({"Test Accuracy": float(test_acc)})
     
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train a model on the Fashion MNIST dataset"
+    )
+    parser.add_argument(
+        "--model_dir", type=str, default=None, help="Directory to store the models",
+    )
+    # training arguments
+    parser.add_argument(
+        "--batch_size", type=int, default=128, help="Batch size for training",
+    )
+    parser.add_argument(
+        "--epochs", type=int, default=10, help="Number of epochs to train for",
+    )
+    parser.add_argument(
+        "--val_split",
+        type=float,
+        default=0.1,
+        help="Validation split for training data",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=133, help="Random seed for training",
+    )
+    parser.add_argument(
+        "--shuffle", type=bool, default=True, help="Shuffle the training data",
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=1e-3, help="Learning rate for training",
+    )
+    parser.add_argument(
+        "--num_layers", type=int, default=2, help="Number of layers in the model",
+    )
+    parser.add_argument(
+        "--hidden_sizes",
+        type=str,
+        default="64",
+        help="this is an integer or a comma-separated list of integers indicating the number of hidden units in each layer of the model.",
+    )
+    parser.add_argument(
+        "--regularizer",
+        type=str,
+        default='l2',
+        help="Regularizer (l1 or l2 or None) to choose for training",
+    )
+    parser.add_argument(
+        "--weight_decay_rate",
+        type=float,
+        default=0,
+        help="Regularizer lambda value",
+    )
+    parser.add_argument(
+        "--optimizer", type=str, default="sgd", help="Optimizer to use for training",
+    )
+    parser.add_argument(
+        "--momentum",
+        type=float,
+        default=0.9,
+        help="Momentum for SGD/rmsprop optimizer",
+    )
+
+    parser.add_argument(
+        "--weight_init",
+        type=str,
+        default="xavier",
+        help="Weight initialization scheme, can be one of 'xavier', 'he', 'uniform', 'random', 'zeros'",
+    )
+    parser.add_argument(
+        "--activation", type=str, default="relu", 
+        help="Activation function to use, can be one of 'relu', 'sigmoid', 'tanh'",
+    )
+    parser.add_argument(
+        "--loss", type=str, default="cross_entropy", help="Loss function to use",
+    )
+    parser.add_argument(
+        "--smoothing_val", type=float, default=0, help="Smoothing value"
+    )
+    parser.add_argument(
+        "--cutmix_prob", type=float, default=0, help="Cutmix Prob"
+    )
+    parser.add_argument(
+        "--mixup_prob", type=float, default=0, help="Mixup Prob"
+    )
+    
+    parser.add_argument(
+        "--augmix_prob", type=float, default=0., help="Augmix Prob"
+    )
+    
+    # add wandb arguments
+    parser.add_argument(
+        "--wandb_project", type=str, default=None, help="W&B project name"
+    )
+    parser.add_argument(
+        "--wandb_entity", type=str, default=None, help="W&B entity name"
+    )
+    parser.add_argument(
+        "--enable_wandb",
+        action="store_true",
+        default=False,
+        help="Whether to enable wandb",
+    )
+    args = parser.parse_args()
+    
+    main(args)
